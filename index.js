204b8bdd3976ef528b5c18dc9acacc26d03daf8d
MD EFTAKHARUL ISLAM KHAN <eftt042@gmail.com>
2025-02-18 21:22:58 +0600

    change to 8 top scholarship

--- index.js
+++ index.js
@@ -179,7 +179,7 @@ async function run() {
           applicationFees: 1,
           scholarshipPostDate: -1,
         })
-        .limit(6)
+        .limit(8)
         .toArray();
 
       res.send(result);
